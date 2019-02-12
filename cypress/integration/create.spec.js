7d0c127c9492a4b7cd455ea498e74b386426473f
euZebe <euzebe@axiome.me>
2019-02-12 20:24:12 +0100

    fix tests

--- cypress/integration/create.spec.js
+++ cypress/integration/create.spec.js
@@ -1,5 +1,5 @@
 describe("From home page", () => {
-  it.only("should be possible to create a new draw", () => {
+  it("should be possible to create a new draw", () => {
     cy.visit("/");
     cy.getByText(/new draw/i).click();
     cy.url().should("eq", `${Cypress.config().baseUrl}/new-draw`);
